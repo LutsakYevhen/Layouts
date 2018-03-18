5461b7aa6f67159a9f75b887c3f023c3cdf44226
Lutsak Yevhen <yev.lutsak@gmail.com>
2018-03-18 14:58:06 +0200

    Upgraded container_linearlayout (portrait version)

--- app/src/main/java/com/example/lutsak/difflayout/MainActivity.java
+++ app/src/main/java/com/example/lutsak/difflayout/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.container_constraintlayout);
+        setContentView(R.layout.container_linearlayout);
     }
 }
